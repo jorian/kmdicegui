f7f2a194816d6c06a7191ffbe85f45bab3482bc7
Jorian <jorian@outlook.com>
2018-10-20 21:14:52 +0200

    initial working gui

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -1,7 +1,10 @@
 package controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 import model.Bet;
 import model.Table;
 import model.TableList;
@@ -14,29 +17,25 @@ import java.math.RoundingMode;
 
 public class Main {
 //    @FXML public Slider oddsSlider;
+    @FXML public Label betAmountLabel;
+    @FXML public Label oddsLabel;
+    @FXML public TextField betAmount;
+    @FXML public TextField odds;
+    @FXML public Label winOrLose;
+
+    Table currentTable;
+
     public void initialize() {
 
+        TableList tableList = new TableList();
+
+
+        // select table in GUI:
+        // getSelected()
 
 
 
-
-
-
-
-
-
-
-
-
-//        TableList tableList = new TableList();
-//
-//
-//        // select table in GUI:
-//        // getSelected()
-//
-//
-//
-//        Table currentTable = tableList.getTables().get(0);
+        currentTable = tableList.getTables().get(0);
 //
 //        // BET should always have 8 decimal places, let GUI guide.
 //        Bet bet = new Bet(currentTable.getName(), currentTable.getFundingTx(), new BigDecimal(1).setScale(8, RoundingMode.DOWN), 2);
@@ -56,5 +55,39 @@ public class Main {
 //            if (result)
 //                System.out.println("main: won: " + bet.getPrize());
 //        }
+    }
+
+    public void bet() {
+        winOrLose.setText("Pending...");
+
+        Bet bet = new Bet(
+                currentTable.getName(),
+                currentTable.getFundingTx(),
+                new BigDecimal(betAmount.getText()).setScale(8, RoundingMode.DOWN),
+                Integer.valueOf(odds.getText())
+        );
+        bet = currentTable.placeBet(bet);
+
+        while (bet.getStatus() == Bet.Status.ONGOING) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (bet.getStatus() == Bet.Status.SUCCESS) {
+            boolean result = bet.won();
+            if (result) {
+                winOrLose.setText("WON " + bet.getPrize());
+                winOrLose.setTextFill(Color.LIGHTGREEN);
+                System.out.println("main: won: " + bet.getPrize());
+            } else {
+                winOrLose.setText("LOST :(");
+                System.out.println("main: lost");
+            }
+        }
+
+
     }
 }
