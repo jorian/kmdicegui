a2c4e600be545aff4c33c12372543dfbe922d519
Jorian <jorian@outlook.com>
2018-10-21 15:09:09 +0200

    add slider

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -1,5 +1,9 @@
 package controller;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 import javafx.concurrent.WorkerStateEvent;
@@ -7,10 +11,13 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.HPos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import model.Bet;
 import model.Table;
@@ -24,27 +31,53 @@ import java.util.ResourceBundle;
 
 
 public class Main implements Initializable {
+    @FXML public GridPane pane;
 //    @FXML public Slider oddsSlider;
     @FXML public Label betAmountLabel;
     @FXML public Label oddsLabel;
     @FXML public TextField betAmount;
-    @FXML public TextField odds;
+
+    IntField odds;
     @FXML public Label winOrLose;
     @FXML public Button betBtn;
+    @FXML public Slider oddsSlider;
 
     Table currentTable;
     private Service<Void> backgroundThread;
 
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
         TableList tableList = new TableList();
         currentTable = tableList.getTables().get(0);
 
+        odds = new IntField(1,currentTable.getMaxOdds(),1);
+        GridPane.setHalignment(odds, HPos.CENTER);
+        GridPane.setColumnIndex(odds, 3);
+        GridPane.setRowIndex(odds, 1);
+        pane.getChildren().add(odds);
+//        <IntField fx:id="odds" GridPane.columnIndex="3" GridPane.rowIndex="1" GridPane.halignment="CENTER"/>
 
+
+        oddsSlider.setMin(1.0);
+        oddsSlider.setMax(currentTable.getMaxOdds());
+        oddsSlider.setShowTickLabels(true);
+        oddsSlider.setMajorTickUnit(currentTable.getMaxOdds());
+//        oddsSlider.tick
+
+        oddsSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
+                if (t1 == null) {
+                    odds.setText("0");
+                }
+
+                odds.setText(Math.round(t1.intValue()) + "");
+            }
+        });
+
+        odds.valueProperty().bindBidirectional(oddsSlider.valueProperty());
     }
 
     @FXML public void bet(ActionEvent event) {
-
         backgroundThread = new Service<Void>() {
             @Override
             protected Task<Void> createTask() {
@@ -52,6 +85,7 @@ public class Main implements Initializable {
                     @Override
                     protected Void call() {
                         updateMessage("Pending...");
+                        winOrLose.setTextFill(Color.BLACK);
 
                         Bet bet = new Bet(
                                 currentTable.getName(),
@@ -84,7 +118,6 @@ public class Main implements Initializable {
                                 winOrLose.setTextFill(Color.RED);
                                 System.out.println("main: lost");
                             }
-
                         }
                         return null;
                     }
@@ -97,5 +130,92 @@ public class Main implements Initializable {
         winOrLose.textProperty().bind(backgroundThread.messageProperty());
 
         backgroundThread.restart();
+    }
+
+    class IntField extends TextField {
+        final private IntegerProperty value;
+        final private int minValue;
+        final private int maxValue;
+
+        // expose an integer value property for the text field.
+        public int  getValue()                 { return value.getValue(); }
+        public void setValue(int newValue)     { value.setValue(newValue); }
+        public IntegerProperty valueProperty() { return value; }
+
+        IntField(int minValue, int maxValue, int initialValue) {
+            if (minValue > maxValue)
+                throw new IllegalArgumentException(
+                        "IntField min value " + minValue + " greater than max value " + maxValue
+                );
+            if (maxValue < minValue)
+                throw new IllegalArgumentException(
+                        "IntField max value " + minValue + " less than min value " + maxValue
+                );
+            if (!((minValue <= initialValue) && (initialValue <= maxValue)))
+                throw new IllegalArgumentException(
+                        "IntField initialValue " + initialValue + " not between " + minValue + " and " + maxValue
+                );
+
+            // initialize the field values.
+            this.minValue = minValue;
+            this.maxValue = maxValue;
+            value = new SimpleIntegerProperty(initialValue);
+            setText(initialValue + "");
+
+            final IntField intField = this;
+
+            // make sure the value property is clamped to the required range
+            // and update the field's text to be in sync with the value.
+            value.addListener(new ChangeListener<Number>() {
+                @Override public void changed(ObservableValue<? extends Number> observableValue, Number oldValue, Number newValue) {
+                    if (newValue == null) {
+                        intField.setText("");
+                    } else {
+                        if (newValue.intValue() < intField.minValue) {
+                            value.setValue(intField.minValue);
+                            return;
+                        }
+
+                        if (newValue.intValue() > intField.maxValue) {
+                            value.setValue(intField.maxValue);
+                            return;
+                        }
+
+                        if (newValue.intValue() == 0 && (textProperty().get() == null || "".equals(textProperty().get()))) {
+                            // no action required, text property is already blank, we don't need to set it to 0.
+                        } else {
+                            intField.setText(newValue.toString());
+                        }
+                    }
+                }
+            });
+
+            // restrict key input to numerals.
+            this.addEventFilter(KeyEvent.KEY_TYPED, new EventHandler<KeyEvent>() {
+                @Override public void handle(KeyEvent keyEvent) {
+                    if (!"0123456789".contains(keyEvent.getCharacter())) {
+                        keyEvent.consume();
+                    }
+                }
+            });
+
+            // ensure any entered values lie inside the required range.
+            this.textProperty().addListener(new ChangeListener<String>() {
+                @Override public void changed(ObservableValue<? extends String> observableValue, String oldValue, String newValue) {
+                    if (newValue == null || "".equals(newValue)) {
+                        value.setValue(0);
+                        return;
+                    }
+
+                    final int intValue = Integer.parseInt(newValue);
+
+                    if (intField.minValue > intValue || intValue > intField.maxValue) {
+                        textProperty().setValue(oldValue);
+                    }
+
+                    value.set(Integer.parseInt(textProperty().get()));
+                }
+            });
+        }
     }
 }
