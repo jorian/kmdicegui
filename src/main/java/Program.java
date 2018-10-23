3890ac3b0eb856e98fe8f47e58df51ceb28cd4fe
Jorian <jorian@outlook.com>
2018-10-23 21:55:19 +0200

    version bump, troubleshooting jar build

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -3,19 +3,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import util.KomodoRPC;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class Program extends Application {
-    ExecutorService threadPool = Executors.newWorkStealingPool();
-
-
-
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/main.fxml"));
         primaryStage.setTitle("KMDice");
         Scene scene = new Scene(root, 800, 600);
         scene.getStylesheets().add("css/style.css");
